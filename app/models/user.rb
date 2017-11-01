0880e6860be49133c38c4a44d81b190e26ad0185
pkbanks <pkbanks@gmail.com>
2017-11-01 15:21:54 -0400

    added model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	has_many :comments
+	# assocation: has and belongs to many
+	has_and_belongs_to_many :followers,
+		class_name: "User",
+		join_table: :followers,
+		foreign_key: :user_id,
+		assocation_foreign_key: :follower_user_id
 end
